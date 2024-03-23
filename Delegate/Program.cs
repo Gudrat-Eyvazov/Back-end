ee87dd6bd881cf09cd07f910864dadbfe9b17564
Gudrat-Eyvazov <gudrateyvazov@gmail.com>
2024-03-24 03:10:46 +0400

    task-16

---
+++ Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate bool Checker(int a);
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine( sum(isodd, 5, 7, 12, 8));
+        }
+        public static int sum(Checker check, params int[] numbers)
+        {
+            int cemi = 0;
+            foreach (int i in numbers)
+            {
+                if(check(i))
+                {
+                    cemi += i;
+                }
+            }
+          return cemi;
+        }
+
+        public static bool isodd ( int number)
+        {
+                if(number%2==1)
+                {
+                    return true;
+                }
+            
+            return false;
+        }
+        public static bool iseven( int number)
+        {           
+                if (number % 2 == 1)
+                {
+                   return true;
+                }
+            return false;
+        }
+    }
+}
